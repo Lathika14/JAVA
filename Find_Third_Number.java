6d3519a0fe8f2c1797a6770760df4339beb19709
Lathika14 <146421018+Lathika14@users.noreply.github.com>
2024-03-27 17:10:45 +0530

    Find_Third_Number.java

---
+++ Find_Third_Number.java
@@ -0,0 +1,19 @@
+public class findThirdNum {
+    public static void main(String args[]) {
+        int[] a = {1, 2, 5, 6, 3, 4};
+        int total = a.length;
+        int temp;
+
+        for (int i = 0; i < total; i++) {
+            for (int j = i + 1; j < total; j++) {
+                if (a[i] > a[j]) {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }             
+            }                
+        }
+
+        System.out.println(a[total - 3]); 
+}
+}
